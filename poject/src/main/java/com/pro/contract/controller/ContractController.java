1dfac97be58f37d11a9686551cb9c468d23276bc
1970805268 <1970805268@qq.com>
2020-11-12 16:14:41 +0800

    ....

--- poject/src/main/java/com/pro/contract/controller/ContractController.java
+++ poject/src/main/java/com/pro/contract/controller/ContractController.java
@@ -1,7 +1,10 @@
 package com.pro.contract.controller;
 
+import com.pro.contract.service.ContractService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author wang
@@ -9,8 +12,44 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @create 2020-11-06  14:01
  */
 @Controller
-@RequestMapping("/contract")
 public class ContractController {
+
+    private ContractService contractService;
+
+    @RequestMapping("/Contoract")
+    public  String deleteByPrimaryKey(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+
+    @RequestMapping("/Contoract")
+    public  String insert(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+    @RequestMapping("/Contoract")
+    public  String insertSelective(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+    @RequestMapping("/Contoract")
+    public  String selectByPrimaryKey(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+    @RequestMapping("/Contoract")
+    public  String updateByPrimaryKeySelective(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+    @RequestMapping("/Contoract")
+    public  String updateByPrimaryKey(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+    @RequestMapping("/Contoract")
+    public  String selectContract(HttpServletRequest req){
+        return  "redirect:/Contoract/list";
+    }
+
+    @RequestMapping("/Contoract")
+    public  String selectContractLike(HttpServletRequest req){
+        return  "redirectList";
+    }
 
 
 }
